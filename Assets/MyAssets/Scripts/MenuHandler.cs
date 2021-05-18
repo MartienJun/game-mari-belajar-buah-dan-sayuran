f6a06640120446894d99fa270724e689c50f2393
MartienJun <martienjunaedi123@gmail.com>
2021-05-18 18:05:33 +0700

    feat: Add script for menu handler

---
+++ Assets/MyAssets/Scripts/MenuHandler.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuHandler : MonoBehaviour
+{
+    public GameObject AboutMenu; 
+    public GameObject ExitMenu;
+    public string UrlGithub = "https://github.com/MartienJun";
+
+    public void ExitApp() 
+    { 
+        Application.Quit();
+    } 
+    
+    public void ChangeScreen(string scene_name) 
+    { 
+        SceneManager.LoadScene(scene_name); 
+    } 
+    
+    public void AboutClicked() 
+    {
+        AboutMenu.SetActive(true);
+        ExitMenu.SetActive(false);
+    }
+
+    public void ExitClicked()
+    {
+        AboutMenu.SetActive(false);
+        ExitMenu.SetActive(true);
+    }
+
+    public void BackClicked() 
+    {
+        AboutMenu.SetActive(false);
+        ExitMenu.SetActive(false);
+    }
+
+    public void GithubClicked()
+    {
+        Application.OpenURL(UrlGithub);
+    }
+}
