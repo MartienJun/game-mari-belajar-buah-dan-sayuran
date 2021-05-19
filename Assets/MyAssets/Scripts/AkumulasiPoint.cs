a141f60f2a32412fd2426fd4fa90a05ff911ea85
MartienJun <martienjunaedi123@gmail.com>
2021-05-19 11:51:30 +0700

    feat: Finalize game

--- Assets/MyAssets/Scripts/AkumulasiPoint.cs
+++ Assets/MyAssets/Scripts/AkumulasiPoint.cs
@@ -9,27 +9,44 @@ public class AkumulasiPoint : MonoBehaviour
     public GameObject titlePanel;
     public GameObject questionPanel;
     public GameObject[] listSoal;
+    public GameObject[] listFruits;
     public GameObject endPanel;
     public Text labelScore;
     GameObject currentQuestion;
+    GameObject currentFruit;
     
     Stack<GameObject> stackSoal = new Stack<GameObject>();
+    Stack<GameObject> stackFruit = new Stack<GameObject>();
 
 
     public void StartGame()
     {
         score = 0;
         titlePanel.SetActive(false);
+        endPanel.SetActive(false);
         questionPanel.SetActive(true);
         List<GameObject> list = new List<GameObject>(listSoal);
+        List<GameObject> list2 = new List<GameObject>(listFruits);
         
         while(list.Count > 0)
         {
             int index = Mathf.FloorToInt(Random.Range(0,list.Count));
             stackSoal.Push(list[index]);
+            stackFruit.Push(list2[index]);
             list.RemoveAt(index);
+            list2.RemoveAt(index);
         }
+        Debug.Log("StartGame");
         NextQuestion();
+    }
+
+    public void EndGame()
+    {
+        endPanel.SetActive(true);
+        questionPanel.SetActive(false);
+        stackSoal.Clear();
+        stackFruit.Clear();
+        labelScore.text = score.ToString();
     }
 
     public void NextQuestion()
@@ -37,24 +54,26 @@ public class AkumulasiPoint : MonoBehaviour
         if(currentQuestion != null)
         {
             currentQuestion.SetActive(false);
+            currentFruit.SetActive(false);
         }
         
         if(stackSoal.Count == 0)
         {
-            endPanel.SetActive(true);
-            labelScore.text = "Score: " + score.ToString();
+            EndGame();
         }
         else
         {
             currentQuestion = stackSoal.Pop();
+            currentFruit = stackFruit.Pop();
             currentQuestion.SetActive(true);
+            currentFruit.SetActive(true);
         }
     }
 
     public void AnswerCorrect()
     {
+        score += 20;
         NextQuestion();
-        score += 1;
     }
 
     public void AnswerWrong()
