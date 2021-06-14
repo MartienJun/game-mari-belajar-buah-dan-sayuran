e92a535e78c5d35d22667cbcea9ba9209e4bb254
MartienJun <martienjunaedi123@gmail.com>
2021-06-14 13:25:29 +0700

    feat: add main menu bgm and button bgm toggler

---
+++ Assets/MyAssets/Scripts/BGMToggler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class BGMToggler : MonoBehaviour
+{
+    public Sprite toggleOnSprite;
+    public Sprite toggleOffSprite;
+    public AudioSource bgm;
+    private bool isToggled = true;
+    private Image image;
+    
+    void Start()
+    {
+        image = GetComponent<Image>();
+        UpdateSprite();
+    }
+
+    public void setBgmButton()
+    {
+        isToggled =! isToggled;
+        UpdateSprite();
+    }
+
+    void UpdateSprite()
+    {
+        if(isToggled)
+        {
+            image.sprite = toggleOnSprite;
+            bgm.UnPause();
+        }
+        else
+        {
+            image.sprite = toggleOffSprite;
+            bgm.Pause();
+        }
+    }
+}
