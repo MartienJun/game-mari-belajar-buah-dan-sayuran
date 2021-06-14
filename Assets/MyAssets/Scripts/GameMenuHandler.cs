76da0423a1dd02420f1b655a822e3a7840bd962b
MartienJun <martienjunaedi123@gmail.com>
2021-06-14 16:04:40 +0700

    fix: menu can't click menu when question is running

---
+++ Assets/MyAssets/Scripts/GameMenuHandler.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenuHandler : MonoBehaviour
+{
+    public GameObject Menu;
+    public GameObject Question;
+    private bool flagActive = false;
+
+    public void ChangeScreen(string scene_name) 
+    { 
+        SceneManager.LoadScene(scene_name); 
+    } 
+    
+    public void MenuClicked() 
+    {
+        if(Question.activeSelf.Equals(true))
+        {
+            Menu.SetActive(true);
+            Question.SetActive(false);
+            flagActive = true;
+        }
+        else
+        {
+            Menu.SetActive(true);
+        }
+    }
+
+    public void BackClicked() 
+    {
+        if(flagActive == true)
+        {
+            Menu.SetActive(false);
+            Question.SetActive(true);
+        }
+        else
+        {
+            Menu.SetActive(false);
+        }
+    }
+}
